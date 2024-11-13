766b425eeda6343c5d888259d14bc8b736038e49
manojLovesCoding <mkum9417@gmail.com>
2024-11-13 17:31:20 +0530

    connected user_auth() to submit button

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,6 +10,16 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
 
+    const user_auth = async (event) => {
+        event.preventDefault();
+        if (signState === "Sign In") {
+            await login(email, password);
+        } else {
+            await signup(name, email, password);
+        }
+
+    }
+
     return (
         <div className='login'>
             <img src={logo} className='login-logo' />
@@ -17,10 +27,10 @@ const Login = () => {
                 <h1>{signState}</h1>
                 <form>
                     {signState === "Sign Up" ?
-                        <input value={name} onChange={(e) => { e.target.value }} type='text' placeholder='Your name' /> : <></>}
-                    <input value={email} onChange={(e) => { e.target.value }} type='email' placeholder='Email' />
-                    <input value={password} onChange={(e) => { e.target.value }} type='password' placeholder='Password' />
-                    <button>{signState}</button>
+                        <input value={name} onChange={(e) => { setName(e.target.value) }} type='text' placeholder='Your name' /> : <></>}
+                    <input value={email} onChange={(e) => { setEmail(e.target.value) }} type='email' placeholder='Email' />
+                    <input value={password} onChange={(e) => { setPassword(e.target.value) }} type='password' placeholder='Password' />
+                    <button onClick={user_auth} type='submit'>{signState}</button>
                     <div className='form-help'>
                         <div className='remember'>
                             <input type='checkbox' />
