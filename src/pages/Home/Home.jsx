e76d50cc98cd21a4829382814dbc5b29547d2c67
manojLovesCoding <mkum9417@gmail.com>
2024-11-13 11:36:37 +0530

    completed navgation bar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+
+export const Home = () => {
+    return (
+        <div className='home'>
+            <Navbar />
+        </div>
+    )
+}
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Player.css'
+
+export const Player = () => {
+    return (
+        <div className='player'>Player</div>
+    )
+}
